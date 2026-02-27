3db4967ac92d2252b70d123339411691853296f5
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-02-27 23:42:24 +0530

    Layout Header 1.O

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,8 @@ import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen text-red-500">
-      
-      <Button>Click me</Button>
+    <div className="flex min-h-screen home-wrapper">
+      Home
     </div>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 header'>
+      <div className='container header-wrapper'>
+          <Link href="/">
+            <Image src="/assets/icons/logo.svg" alt="Binivex Logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
+          </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
