efe99ba05ff2c5c93bece566d5a5970caa94aa82
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-03-04 17:35:37 +0530

    header,navbar & dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
+import NavItems from './NavItems';
+import UserDropdown from './UserDropdown';
 
 
 const Header = () => {
@@ -10,6 +12,11 @@ const Header = () => {
           <Link href="/">
             <Image src="/assets/icons/logo.svg" alt="Binivex Logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
           </Link>
+          <nav className='hidden sm:block'>
+            {/*nav items*/}
+            <NavItems/>
+          </nav>
+          <UserDropdown/>
       </div>
     </header>
   )
