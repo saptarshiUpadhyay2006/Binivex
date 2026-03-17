2d3056cfc8c27359d24c0b5d0e340814ca0d3a03
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-03-17 22:42:54 +0530

    Auth 2.O

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
+'use client';
 import { useForm } from 'react-hook-form';
+import {Button} from "@/components/ui/button";
 
 const SignUp = () => {
     const {
         register,
         handleSubmit,
-        watch,
-        formState: { errors },
-      } = useForm<SignUpFormData>()
-    //   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+        control,
+        formState: { errors,isSubmitting},
+      } = useForm<SignUpFormData>({
+        defaultValues: {
+          fullName: '',
+          email: '',
+          password: '',
+          country: 'US',
+          investmentGoals: 'Growth',
+          riskTolerance: 'Medium',
+          preferredIndustry: 'Technology'
+      },
+      mode: 'onBlur'
+  }, );
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data);
+        // const result = await signUpWithEmail(data);
+        // if(result.success) router.push('/');
+    } catch (e) {
+        console.error(e);
+        // toast.error('Sign up failed', {
+        //     description: e instanceof Error ? e.message : 'Failed to create an account.'
+        // })
+    }
+}
   return (
     <>
         <h1 className='form-title'>Sign Up & Personalize</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+          <Button type="submit" disabled={isSubmitting} className='yellow-btn w-full mt-5'>
+              {isSubmitting?'Creating Account':'Start Your Investing Journey'}
+          </Button>
+        </form>
     </>
   )
 }
