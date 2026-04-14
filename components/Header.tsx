ba2009526069fa258110450fd1bc1fcc7649805e
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-04-14 20:34:51 +0530

    Search feature

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 import Image from "next/image";
 import NavItems from './NavItems';
 import UserDropdown from './UserDropdown';
+import { searchStocks } from '@/lib/actions/finnhub.actions';
 
 
-const Header = ({user}:{user:User}) => {
+const Header =async ({user}:{user:User}) => {
+
+  const initialStocks=await searchStocks();
   return (
     <header className='sticky top-0 header'>
       <div className='container header-wrapper'>
@@ -16,9 +19,9 @@ const Header = ({user}:{user:User}) => {
           </Link>
           <nav className='hidden sm:block'>
             {/*nav items*/}
-            <NavItems/>
+            <NavItems initialStocks={initialStocks}/>
           </nav>
-          <UserDropdown user={user}/>
+          <UserDropdown user={user} initialStocks={initialStocks}/>
       </div>
     </header>
   )
